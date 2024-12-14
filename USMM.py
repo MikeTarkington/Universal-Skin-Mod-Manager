52e125e7ac8db90ece74878387d98c6feff625be
Mike Tarkington <tarkmike@gmail.com>
2024-12-14 15:50:01 -0800

    Update USMM.py
    Add game class for form entries to add games once db is available. Add form display components to UI.

--- USMM.py
+++ USMM.py
@@ -31,27 +31,41 @@ modables = ["Raiden","Shenhe", "Jean", "Barbara", "Keqing", "Chasca"]
 
 # business logic
 
+class Game:
+    def __init__(self, title, applied_path, store_path):
+        self.title = title
+        self.applied_path = applied_path
+        self.store_path = store_path
+
+def add_game():
+    title = game_t.get()
+    applied_path = game_mods_path.get()
+    store_path = game_modables_path.get()
+    game = Game(title, applied_path, store_path)
+    print(vars(game))
+    games.append(game.title) # will eventually write to db or conf file
+    # need to update the game list page somehow to show the newly added game
+
+# consider refactor to join with display_modables**
 def set_game_list():
     i = 0
     for game in games:
         i += 1
         game_listb.insert(i, game)
 
+# consider refactor to join with set_game_list**
 def display_modables(game):
     i = 0
     for modable in modables:
         i += 1
-        modable_listb.insert(i, modable)
+        modables_list_lb.insert(i, modable)
 
 
 # display content:
 
-# button to add game
-b_add_game = ttk.Button(main_frame, text="Add Game")
-b_add_game.grid(column=1, row=3, padx=5, pady=5)
-
+# game list display
 game_frame = ttk.Frame(main_frame, padding="3 3 12 12").grid()
-game_list_l = ttk.Label(main_frame, text='Game List')
+game_list_l = ttk.Label(main_frame, text='Games')
 game_list_l.grid(column=1, row=1, padx=5, pady=5)
 game_listb = Listbox(main_frame, listvariable=games, selectmode=SINGLE, height=10)
 game_listb.grid(column=1, row=2, padx=5, pady=5)
@@ -59,8 +73,33 @@ game_listb.grid(column=1, row=2, padx=5, pady=5)
 # pupulate the games list by finding them in db (WIP)
 set_game_list()
 
-modable_listb = Listbox(main_frame, listvariable=modables, selectmode=SINGLE, height=10)
-modable_listb.grid(column=2, row=2, padx=5, pady=5)
+# form to add game
+game_title_l = ttk.Label(main_frame, text='Game Title')
+game_title_l.grid(column=1, row=3, padx=5, pady=5)
+game_t = StringVar()
+game_t = ttk.Entry(main_frame, textvariable=game_t)
+game_t.grid(column=1, row=4, padx=5, pady=5)
+game_path_l = ttk.Label(main_frame, text='Path to Applied Mods Folder')
+game_path_l.grid(column=1, row=5, padx=5, pady=5)
+# add browse button option for entering path**
+game_modables_path = StringVar()
+game_modables_path = ttk.Entry(main_frame, textvariable=game_modables_path)
+game_modables_path.grid(column=1, row=6, padx=5, pady=5)
+game_path_l = ttk.Label(main_frame, text='Path to Mod Storage Folder')
+game_path_l.grid(column=1, row=7, padx=5, pady=5)
+# add browse button option for entering path**
+game_mods_path = StringVar()
+game_mods_path = ttk.Entry(main_frame, textvariable=game_mods_path)
+game_mods_path.grid(column=1, row=8, padx=5, pady=5)
+# submit form
+b_add_game = ttk.Button(main_frame, text="Add Game", command=add_game)
+b_add_game.grid(column=1, row=9, padx=5, pady=5)
+
+# modable list display
+modables_listl = ttk.Label(main_frame, text='Assets (Char Skins, etc)')
+modables_listl.grid(column=2, row=1, padx=5, pady=5)
+modables_list_lb = Listbox(main_frame, listvariable=modables, selectmode=SINGLE, height=10)
+modables_list_lb.grid(column=2, row=2, padx=5, pady=5)
 
 # click a game to show modables for that game in its corresponding listbox
 game_listb.bind('<<ListboxSelect>>', display_modables)
