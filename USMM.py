eb3a685d88e1c2e2aebb316ee3da19a2dd33d53d
Mike Tarkington <tarkmike@gmail.com>
2025-01-24 00:34:20 -0800

    Add MVP functionality for mod activation

--- USMM.py
+++ USMM.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import sqlite3
 import ttkbootstrap as ttk
 from tkinter import *
@@ -28,6 +29,7 @@ main_frame = ttk.Frame(root, padding="3 3 12 12")
 main_frame.grid(column=0, row=0, padx=5, pady=5, sticky=(N, W, E, S))
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
+main_frame.columnconfigure((1, 2, 3, 4), minsize=250)
 
 # setup default example data for db and create local folders for the game paths
 curr_path = f"{os.getcwd()}"
@@ -54,6 +56,9 @@ if cur.execute("SELECT 1 FROM game LIMIT 1").fetchone() == None:
 
 
 # BUSINESS LOGIC
+current_selected_game = []
+current_selected_modable = ""
+
 
 class Game:
     def __init__(self, title, applied_path, store_path):
@@ -72,24 +77,26 @@ def add_game():
     con.commit()
     game_list_lb.insert(ttk.END, game.title)
     return game
-
-    # may need to close the db connection at some point after committing but
+    # - may need to close the db connection at some point after committing but
     #  would have reopen later for other tasks potentially so not sure yet
     # con.close()
-    
-    # how I might call on a particular game using its title:
-    # game_query = cur.execute("""SELECT title, appliedPath, storePath 
-    #                          FROM game WHERE title=?""", (title,))
-    # print(game_query.fetchone())
-    
-    # need to update the game list page somehow to show the newly added game
-    # clear form fields after submission or make a separate dialog that closes
+    # - clear form fields after submission or make a separate dialog that closes
 
-# consider refactor to join with display_modables**
+def delete_game():
+    print("delete game")
+
+# write this function to fill in the entry fields of the add game form with paths
+def browse_folder():
+    folder_path = filedialog.askdirectory()
+    if folder_path:
+        print(folder_path)
+        # entry.delete(0, tk.END)
+        # entry.insert(0, folder_path)
+
 def set_game_list():
     games_l_query = cur.execute("SELECT title FROM game").fetchall()
     game_list_b = []
-    i = 0 #consider refactor of insertions to use ttk.END insead of index
+    i = 0
     for game in games_l_query:
         game_list_b.insert(i, game[0])
         i += 1
@@ -97,40 +104,106 @@ def set_game_list():
     return game_titles
 
 # Returns a list of file paths for folders within the specified directory
-def get_modables_paths(folder_path):
-    modables_paths = []
+def get_folder_paths(folder_path):
+    paths = []
     for modable in os.scandir(folder_path):
         if modable.is_dir(): #after adding form validation should be able to remove condition
-            modables_paths.append(modable.path)
-    return modables_paths
+            paths.append(modable.path)
+    return paths
 
 def display_modables(selected_game=()):
     modables_list_lb.delete(0, ttk.END)
+    mods_list_lb.delete(0, ttk.END)
     selected_game = game_list_lb.curselection()
-    if selected_game == (): #consider whether this is still necessary if we have default selection
+    if selected_game == (): #consider whether this is still necessary if using default selection
         selected_title = game_list_lb.get(0)
     else:
         selected_title = game_list_lb.get(selected_game)
-    print("*&*&*&*&*&*&*&")
-    print(selected_game)
-    print(selected_title)
-    game = cur.execute("SELECT appliedPath FROM game WHERE title=?", (selected_title,)).fetchone()
-    modables = get_modables_paths(game[0])
-    i = 0 #consider refactor of insertions to use ttk.END insead of index
+    game = cur.execute("SELECT storePath, appliedPath FROM game WHERE title=?", (selected_title,)).fetchone()
+    modables = get_folder_paths(game[0])
+    i = 0
     for modable in modables:
         modables_list_lb.insert(i, modable.rsplit("\\", 1)[-1])
         i += 1
-    print("*&*&*&*&*&*&*&")
-    print(modables_list_lb)
     modables_list = ttk.Variable(value=modables_list_lb)
+    global current_selected_game
+    current_selected_game = game
+    active_mods_display(current_selected_game[1])
     return modables_list
 
-def browse_folder():
-    folder_path = filedialog.askdirectory()
-    if folder_path:
-        print(folder_path)
-        # entry.delete(0, tk.END)
-        # entry.insert(0, folder_path)
+def refresh_modables():
+    print("refresh please")
+
+def display_mods(selected_modable=()):
+    mods_list_lb.delete(0, ttk.END)
+    selected_modable = modables_list_lb.curselection()
+    if selected_modable == (): #consider whether this is still necessary if using default selection
+        selected_modable = modables_list_lb.get(0)
+    else:
+        selected_modable = modables_list_lb.get(selected_modable)
+    print(selected_modable)
+    modable_path = f"{current_selected_game[0]}\\{selected_modable}"
+    print(modable_path)
+    mods = get_folder_paths(modable_path)
+    i = 0
+    for mod in mods:
+        mods_list_lb.insert(i, mod.rsplit("\\", 1)[-1])
+        i += 1
+    mods_list = ttk.Variable(value=mods_list_lb)
+    global current_selected_modable
+    current_selected_modable = modable_path
+    return mods_list
+    
+def activate_mod(mod=()):
+    # print(current_selected_modable)
+    active_path = current_selected_game[1]
+    # print(active_path)
+    mod_selection = mods_list_lb.curselection()
+    mod = mods_list_lb.get(mod_selection[0])
+    mod_path = f"{current_selected_modable}\\{mod}"
+    print(mod_path)
+    active = active_mod(current_selected_modable, active_path)
+    print("**********")
+    print(active)
+    # stuff here to activate game by removing the currently active one and copying in the selected
+    if os.path.exists(active):
+        shutil.rmtree(active)
+    else:
+        print(active) # consider error handling
+    destination_path = f"{active_path}\\{mod}"
+    print("+++++++++++++")
+    print(destination_path)
+    shutil.copytree(mod_path, destination_path)
+    active_mods_display(active_path)
+    return "activated"
+
+
+def active_mod(modables_path, active_mods_path):
+    active_mod = "no active mod for modable"
+    mod_folder_name = ""
+    check_path = ""
+    for mod in os.scandir(modables_path):
+        mod_folder_name = mod.path.rsplit("\\", 1)[-1]
+        check_path = f"{active_mods_path}\\{mod_folder_name}"
+        if os.path.exists(check_path):
+            active_mod = check_path
+    return active_mod
+
+def active_mods_display(active_mods_path):
+    active_mods_list_lb.delete(0, ttk.END)
+    mod_paths = get_folder_paths(active_mods_path)
+    i = 0
+    for path in mod_paths:
+        active_mods_list_lb.insert(i, path.rsplit("\\", 1)[-1])
+        i += 1
+    mods_list = ttk.Variable(value=mods_list_lb)
+    return mods_list
+
+def deactivate_mod(event):
+    print("deactive modable")
+
+def display_mod_info(mod=()):
+    print("mod info")
 
 
 
@@ -140,7 +213,7 @@ def browse_folder():
 # look into adding entry field validation
     # https://tkdocs.com/tutorial/widgets.html#entry:~:text=leads%20us%20to...-,Validation,-Users%20can%20type
 add_game_frame = ttk.Frame(main_frame, padding="3 3 10 3")
-add_game_frame.grid(column=1, row=3)
+add_game_frame.grid(column=1, row=4)
 game_title_l = ttk.Label(add_game_frame, text='Game Title')
 game_title_l.grid(column=1, sticky=(W), columnspan=2, row=1, padx=5, pady=5)
 game_t = StringVar()
@@ -173,20 +246,64 @@ game_titles = set_game_list()
 # game_frame.grid()
 game_list_l = ttk.Label(main_frame, text='Games')
 game_list_l.grid(column=1, row=1, padx=5, pady=5)
-game_list_lb = Listbox(main_frame, listvariable=game_titles, selectmode=SINGLE, height=10)
+game_list_lb = Listbox(main_frame, 
+                       listvariable=game_titles, 
+                       selectmode=SINGLE, 
+                       height=10,
+                       exportselection=False
+                       )
 game_list_lb.grid(column=1, row=2, padx=5, pady=5)
-# click a game to show modables for that game in its corresponding listbox
-game_list_lb.selection_set( first = 0 )
+game_list_lb.selection_set(first=0)
 game_list_lb.bind('<<ListboxSelect>>', display_modables)
+remove_game = ttk.Button(main_frame, text="Remove Game", command=delete_game)
+remove_game.grid(column=1, row=3, padx=5, pady=5, sticky=N)
 
 # modable list display
 modables_list = ttk.Variable(value=[])
-modables_listl = ttk.Label(main_frame, text='Assets (Char Skins, etc)')
+modables_listl = ttk.Label(main_frame, text='Modable Assets (Char Skins, etc)')
 modables_listl.grid(column=2, row=1, padx=5, pady=5)
 modables_list_lb = Listbox(main_frame,
                            listvariable=modables_list,
-                             selectmode=SINGLE, height=10)
+                           selectmode=SINGLE,
+                           height=10,
+                           exportselection=False
+                           )
 modables_list_lb.grid(column=2, row=2, padx=5, pady=5)
+modables_list_lb.selection_set(first=0)
+modables_list_lb.bind('<<ListboxSelect>>', display_mods)
+refresh_modables_b = ttk.Button(main_frame, text="Refresh List", command=refresh_modables)
+refresh_modables_b.grid(column=2, row=3, padx=5, pady=5, sticky=N)
 
+# mods for modable list display
+mods_list = ttk.Variable(value=[])
+mods_listl = ttk.Label(main_frame, text='Mods for Asset')
+mods_listl.grid(column=3, row=1, padx=5, pady=5)
+mods_list_lb = Listbox(main_frame,
+                           listvariable=mods_list,
+                           selectmode=SINGLE,
+                           height=10,
+                           exportselection=False
+                           )
+mods_list_lb.grid(column=3, row=2, padx=5, pady=5)
+mods_list_lb.selection_set(first=0)
+mods_list_lb.bind('<<ListboxSelect>>', display_mod_info)
+activate_mod_b = ttk.Button(main_frame, text="Activate", command=activate_mod)
+activate_mod_b.grid(column=3, row=3, padx=5, pady=5, sticky=NW)
+deactivate_b = ttk.Button(main_frame, text="Deactivate", command=deactivate_mod)
+deactivate_b.grid(column=3, row=3, padx=5, pady=5, sticky=NE)
+
+# active mods for game list display
+active_mods_list = ttk.Variable(value=[])
+active_mods_list_l = ttk.Label(main_frame, text='Active Mods')
+active_mods_list_l.grid(column=4, row=1, padx=5, pady=5)
+active_mods_list_lb = Listbox(main_frame, 
+                       listvariable=active_mods_list, 
+                       selectmode=SINGLE, 
+                       height=10,
+                       exportselection=False
+                       )
+active_mods_list_lb.grid(column=4, row=2, padx=5, pady=5)
+active_mods_list_lb.selection_set(first=0)
+active_mods_list_lb.bind('<<ListboxSelect>>', display_mod_info)
 
 root.mainloop()
